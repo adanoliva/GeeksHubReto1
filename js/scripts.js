e05c3dbc3ede2f5bdf6e59b2e2240356be856084
ADAN <trafuga@gmail.com>
2021-03-13 11:46:34 +0100

    Añadido localstorage para guardar la página que estamos visitando

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
+window.onload = function() {
+    let ls =localStorage.getItem("enlace")
+    if (ls != null){
+        if (ls == 'inicio')
+        {
+            cargainicio() 
+        }
+        else
+        {
+            cargacontacto() 
+        }
+    }
+  };
+
+
 function cargainicio() 
 {
     document.getElementById("listado").style.display = ""
     document.getElementById("contacto").style.display = "none"
     document.getElementById("lcontacto").classList.remove("linkActivo")
     document.getElementById("linicio").classList.add("linkActivo")
+    localStorage.setItem("enlace", "inicio")
 }
 
 function cargacontacto() 
@@ -12,4 +28,6 @@ function cargacontacto()
     document.getElementById("contacto").style.display = ""
     document.getElementById("lcontacto").classList.add("linkActivo")
     document.getElementById("linicio").classList.remove("linkActivo")
+    localStorage.setItem("enlace", "contacto")
 }
+
